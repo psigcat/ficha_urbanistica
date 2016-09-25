60d230a143baeecf4241474c042bae260d555832
Martí Angelats i Ribera <martiangelats@hotmail.com>
2016-09-25 19:55:20 +0200

    Move credentials file.

---
+++ db_credentials.py
@@ -0,0 +1,7 @@
+"""File with the database credentials."""
+# Do NOT upload the actual credentials (for quite obvious security reasons).
+host     = 
+port     = 
+dbname   = 
+user     = 
+password = 
