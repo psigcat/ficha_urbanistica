6e881ad292a95073abc7f524646c0721b9765105
Martí Angelats i Ribera <martiangelats@hotmail.com>
2016-09-25 14:48:12 +0200

    New data_form based on the DB information (to be finished. More data is required).

--- sql/form_data.sql
+++ sql/form_data.sql
@@ -1,28 +1,57 @@
------------- FINAL RESULT ------------
+-- Funcio que retorna una taula amb tots els valors necesaris pel formulari.
+CREATE OR REPLACE FUNCTION ficha_tecnica(int) RETURNS TABLE(refcat text, area int, adreca text, codi_sector text, descr_sector text, codi_classi text, descr_classi text, codi_zones text[] percent_zones numeric[])
+AS $$
 
+  WITH
+    _parcela AS (
+      SELECT refcat, geom, ST_Area(geom) as area
+      FROM parcela
+      WHERE ninterno = $1
+    )
 
------------- QUERY IN A FUNCTION ------------
--- The plugin doesn't need to know the implementation. Using a function separates te server side from the client side better.
-CREATE OR REPLACE FUNCTION getIntersectingInfo(IN selected geometry)
-AS '
-	SELECT 
-		ARRAY(
-			WITH edif_areas_table AS (
-				SELECT ref, AREA(ST_Multi(ST_Intersection(selected, edif.geo))) AS edif_area
-				FROM edif
-				WHERE ST_Intersects(selected, edif.geo)
-				GROUP BY ref
-			)
-			SELECT ROW(ref, 100 * edif_area / SUM(edif_area) AS percent)
-			WHERE percent >= 3
-			ORDER BY percent DESC
-		) AS resultat_edif,
-		ARRAY(
-			SELECT ref
-				FROM zones
-				WHERE ST_Intersects(selected, zones.geo)
-		) AS resultat_zones
-	;
-'
-LANGUAGE SQL
-;
+  SELECT
+    _parcela.refat AS refcat,
+    _parcela.area AS area,
+    (
+      -- TODO
+    ) AS adreca,
+    _sector.codi AS codi_sector,
+    _sector.descr AS descr_sector,
+    _classi.codi AS codi_classi,
+    _classi.descr AS descr_classi,
+    ARRAY(SELECT codi FROM _zones) AS codi_zones,
+    ARRAY(SELECT percent FROM _zones) AS percent_zones
+
+  FROM
+    _parcela,
+    ( -- Subquery per aconseguir a quin sector partany el terreny
+      SELECT codi, descipcio
+      FROM sectors_urbanistics, _parcela
+      WHERE TS_Intersects(_parcela.geom, sectors_urbanistics.geom)
+      -- Hi pot haver petits errors deguts al mapa. Utilitzem el sector amb més area
+      ORDER BY TS_Area(TS_Intersection(sectors_urbanistics.geom)) DESC
+      LIMIT 1
+    ) AS _sector,
+
+    ( -- Subquery per aconseguir quina calificacio té el terreny
+      SELECT codi, descipcio
+      FROM classificacio, _parcela
+      WHERE TS_Intersects(_parcela.geom, classificacio.geom)
+      -- Hi pot haver petits errors deguts al mapa. Utilitzem el sector amb més area
+      ORDER BY TS_Area(TS_Intersection(classificacio.geom)) DESC
+      LIMIT 1
+    ) AS _classi,
+
+    (
+      -- Aconseguim el parcentatge de l'area i descartem totes les ares que son < 3%
+      SELECT _zones.codi, 100 * TS_Area(_zones.area) / SUN(TS_Area(_zones.area)) AS percent
+      FROM (
+        -- Troba les zones i les areas.
+        -- TODO
+      ) AS _zones
+      WHERE percent >= 3
+      GROUP BY _zones.codi
+      ORDER BY percent DESC
+    ) _zones;
+
+$$ LANGUAGE SQL;
